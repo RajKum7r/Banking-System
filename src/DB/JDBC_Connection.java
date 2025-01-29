ccd2403858d26f682aea4c368b52d931cf0c0c65
Shri-RajKumar <blkhackr1j@gmail.com>
2025-01-30 01:38:50 +0530

    add banking system project folder

---
+++ src/DB/JDBC_Connection.java
@@ -0,0 +1,7 @@
+package DB;
+
+public class JDBC_Connection {
+
+//	Class.forName("com.mysql.jdbc.Driver");
+//    conn= DriverManager.getConnection("jdbc:mysql://localhost:3306/online_examination_system","root","vvit12345");
+}
